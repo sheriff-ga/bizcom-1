e7a89a1ce8456c5a836aca1766c4b394fc3ae856
Sheriff <sheriff.azeez@globalaccelerex.com>
2024-01-08 17:24:02 +0100

    stage 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './About';
+import Home from "./Home";
+import Services from './Services';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/bizcomm" element={<App />} />
+        <Route path="/bizcomm" element={<Home />} />
+        <Route path="/bizcomm/about" element={<About />} />
+        <Route path="/bizcomm/services" element={<Services />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
